fc54b8b1773e075148a3424a7dacb79ffc0e7b88
Portno <j.remoundos@dotbydot.gr>
2019-04-25 14:15:17 +0300

    added extension method

---
+++ src/Panacea.Modularity.MediaPlayerContainer/Extensions/PanaceaServicesExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Panacea.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panacea.Modularity.MediaPlayerContainer.Extensions
+{
+    public static class PanaceaServicesExtensions
+    {
+        public static IMediaPlayerContainer GetMediaPlayerContainer(this PanaceaServices services)
+        {
+            return services.PluginLoader.GetPlugins<IMediaPlayerContainer>().FirstOrDefault();
+        }
+    }
+}
