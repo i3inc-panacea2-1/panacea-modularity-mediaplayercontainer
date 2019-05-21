634baa8a69b0b46666abb21fb5c040bfee213d60
Portno <j.remoundos@dotbydot.gr>
2019-05-21 14:13:56 +0300

    changed api

---
+++ src/Panacea.Modularity.MediaPlayerContainer/PanaceaServicesExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Panacea.Core;
+using System.Linq;
+
+namespace Panacea.Modularity.MediaPlayerContainer
+{
+    public static class PanaceaServicesExtensions
+    {
+        public static IMediaPlayerContainer GetMediaPlayerContainer(this PanaceaServices services)
+        {
+            return services.PluginLoader.GetPlugins<IMediaPlayerContainer>().FirstOrDefault();
+        }
+
+        public static bool TryGetMediaPlayerContainer(this PanaceaServices services, out IMediaPlayerContainer container)
+        {
+            container = null;
+            var plugin = services.PluginLoader.GetPlugins<IMediaPlayerContainer>().FirstOrDefault();
+            if(plugin == null)
+            {
+                return false;
+            }
+            container = plugin;
+            return true;
+        }
+    }
+}
