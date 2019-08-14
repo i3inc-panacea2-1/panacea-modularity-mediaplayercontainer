bd6cb45452a79023fe1b448a477299fcfbe62423
Portno <j.remoundos@dotbydot.gr>
2019-08-14 16:17:32 +0300

    update

--- src/Panacea.Modularity.MediaPlayerContainer/MediaResponse.cs
+++ src/Panacea.Modularity.MediaPlayerContainer/MediaResponse.cs
@@ -7,8 +7,42 @@ using System.Threading.Tasks;
 
 namespace Panacea.Modularity.MediaPlayerContainer
 {
-    public interface IMediaResponse : IMediaPlayer
+    public interface IMediaResponse
     {
         MediaRequest Request { get; }
+
+        bool IsPausable { get; }
+        TimeSpan Duration { get; }
+        bool HasPrevious { get; }
+        bool HasNext { get; }
+        bool IsPlaying { get; }
+        float Position { get; set; }
+        bool IsSeekable { get; }
+        bool HasSubtitles { get; }
+
+        event EventHandler<TimeSpan> DurationChanged;
+        event EventHandler<bool> HasSubtitlesChanged;
+        event EventHandler Opening;
+        event EventHandler Playing;
+        event EventHandler<string> NowPlaying;
+        event EventHandler Stopped;
+        event EventHandler<bool> HasPreviousChanged;
+        event EventHandler Paused;
+        event EventHandler<bool> IsPausableChanged;
+        event EventHandler<bool> HasNextChanged;
+        event EventHandler<Exception> Error;
+        event EventHandler<bool> IsSeekableChanged;
+        event EventHandler Ended;
+        event EventHandler<float> PositionChanged;
+
+        bool CanPlayChannel(object channel);
+        bool HasMoreChapters();
+        void Next();
+        void Pause();
+        void Play();
+        Task Play(MediaItem channel);
+        void Previous();
+        void SetSubtitles(bool on);
+        void Stop();
     }
 }
